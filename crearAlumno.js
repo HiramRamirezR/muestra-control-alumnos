3ce639880aa7b5071457d7d2edda76acc832b3fc
HiramRamirezR <68158101+HiramRamirezR@users.noreply.github.com>
2024-06-11 00:52:35 -0600

    Fix error de duplicidad
    Los alumnos ya no se duplican al guardar cambios varias veces durante una misma edición de datos en el formulario.

--- crearAlumno.js
+++ crearAlumno.js
@@ -11,60 +11,65 @@ onAuthStateChanged(auth, (user) => {
   if (!user) {
     // El usuario no está autenticado, redirigir al formulario de inicio de sesión
     window.location.href = "index.html";
-  } else {
-    // El usuario está autenticado, puedes acceder a su información
-    // console.log('Usuario autenticado:', user);
-    // Aquí puedes realizar las operaciones relacionadas con buscar alumno
   }
 });
 
 const enviarForm = document.getElementById('enviarForm');
 
-enviarForm.addEventListener('click', () => {  
-  const numeroCertificado = document.getElementById('numeroCertificado').value;
-  const radioNino = document.getElementById('radioNino').checked;
-  const radioAdulto = document.getElementById('radioAdulto').checked;
-  const gradoActual = document.getElementById('gradoActual').value;
-  const gradoParaSubir = document.getElementById('gradoParaSubir').value
-  const nombre = document.getElementById('nombres').value
-  const apellidoPaterno = document.getElementById('apellidoPaterno').value
-  const apellidoMaterno = document.getElementById('apellidoMaterno').value
-  const fechaNacimiento = document.getElementById('fechaNacimiento').value;
-  const edad = document.getElementById('edad').value
-  const direccion = document.getElementById('direccion').value
-  const telefono = document.getElementById('telefono').value;
-  const ocupacion = document.getElementById('ocupacion').value;
-  const fechaIngreso = document.getElementById('fechaIngreso').value;
-  const tiempoPracticando = document.getElementById('tiempoPracticando').value
-  const dojang = document.getElementById('dojang').value
-  const nombreProfesor = document.getElementById('nombreProfesor').value
-  const fechaExamenAnterior = document.getElementById('fechaExamenAnterior').value;
-  const fechaExamen = document.getElementById('fechaExamen').value;
+enviarForm.addEventListener('click', () => {
+  // Solo ejecuta el código de creación si no estamos en modo edición
+  const urlParams = new URLSearchParams(window.location.search);
+  const editarAlumnoID = urlParams.get('editar');
 
-  if (nombre) {
-    push(alumnos, {
-      numeroCertificado,
-      radioNino,
-      radioAdulto,
-      gradoActual,
-      gradoParaSubir,
-      nombre,
-      apellidoPaterno,
-      apellidoMaterno,
-      fechaNacimiento,
-      edad,
-      direccion,
-      telefono,
-      ocupacion,
-      fechaIngreso,
-      tiempoPracticando,
-      dojang,
-      nombreProfesor,
-      fechaExamenAnterior,
-      fechaExamen
-    })
-    alert(`${nombre} ${apellidoPaterno} ${apellidoMaterno} ha sido registrado exitosamente.`);
-  } else {
-    alert('Por favor, rellene todos los campos.');
+  if (!editarAlumnoID) {
+    const numeroCertificado = document.getElementById('numeroCertificado').value;
+    const radioNino = document.getElementById('radioNino').checked;
+    const radioAdulto = document.getElementById('radioAdulto').checked;
+    const gradoActual = document.getElementById('gradoActual').value;
+    const gradoParaSubir = document.getElementById('gradoParaSubir').value;
+    const nombre = document.getElementById('nombres').value;
+    const apellidoPaterno = document.getElementById('apellidoPaterno').value;
+    const apellidoMaterno = document.getElementById('apellidoMaterno').value;
+    const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    const edad = document.getElementById('edad').value;
+    const direccion = document.getElementById('direccion').value;
+    const telefono = document.getElementById('telefono').value;
+    const ocupacion = document.getElementById('ocupacion').value;
+    const fechaIngreso = document.getElementById('fechaIngreso').value;
+    const tiempoPracticando = document.getElementById('tiempoPracticando').value;
+    const dojang = document.getElementById('dojang').value;
+    const nombreProfesor = document.getElementById('nombreProfesor').value;
+    const fechaExamenAnterior = document.getElementById('fechaExamenAnterior').value;
+    const fechaExamen = document.getElementById('fechaExamen').value;
+
+    if (nombre) {
+      push(alumnos, {
+        numeroCertificado,
+        radioNino,
+        radioAdulto,
+        gradoActual,
+        gradoParaSubir,
+        nombre,
+        apellidoPaterno,
+        apellidoMaterno,
+        fechaNacimiento,
+        edad,
+        direccion,
+        telefono,
+        ocupacion,
+        fechaIngreso,
+        tiempoPracticando,
+        dojang,
+        nombreProfesor,
+        fechaExamenAnterior,
+        fechaExamen
+      }).then(() => {
+        alert(`${nombre} ${apellidoPaterno} ${apellidoMaterno} ha sido registrado exitosamente.`);
+      }).catch((error) => {
+        console.error('Error al registrar el alumno:', error);
+      });
+    } else {
+      alert('Por favor, rellene todos los campos.');
+    }
   }
-})
+});
--- editarAlumno.js
+++ editarAlumno.js
@@ -2,52 +2,76 @@ import app from './firebaseConfig.js'
 import { getDatabase, ref, onValue, set } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js';
 
 const database = getDatabase(app);
-const alumnos = ref(database, 'alumnos');
 
 // Función para obtener el parámetro 'editar' de la URL
 function obtenerParametroEditar() {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get('editar');
 }
-  
+
 // Verifica si hay un parámetro 'editar'
 const editarAlumnoID = obtenerParametroEditar();
 if (editarAlumnoID) {
     // Si hay un parámetro 'editar', cargar información del alumno
     cargarInformacionEdicion(editarAlumnoID);
 }
-  
+
 function cargarInformacionEdicion(id) {
     let ubicacion = ref(database, `alumnos/${id}`);
-    // console.log(id);
     onValue(ubicacion, (snapshot) => {
-      const alumno = snapshot.val();
-      document.getElementById('numeroCertificado').value = alumno["numeroCertificado"];
-      document.getElementById('gradoActual').value = alumno["gradoActual"];
-      document.getElementById('gradoParaSubir').value = alumno["gradoParaSubir"];
-      document.getElementById('radioNino').checked = alumno["radioNino"];
-      document.getElementById('radioAdulto').checked = alumno["radioAdulto"];
-      document.getElementById('nombres').value = alumno.nombre
-      document.getElementById('apellidoPaterno').value = alumno["apellidoPaterno"];
-      document.getElementById('apellidoMaterno').value = alumno["apellidoMaterno"];
-      document.getElementById('fechaNacimiento').value = alumno["fechaNacimiento"];
-      document.getElementById('edad').value = alumno["edad"];
-      document.getElementById('direccion').value = alumno["direccion"];
-      document.getElementById('telefono').value = alumno["telefono"];
-      document.getElementById('ocupacion').value = alumno["ocupacion"];
-      document.getElementById('fechaIngreso').value = alumno["fechaIngreso"];
-      document.getElementById('tiempoPracticando').value = alumno["tiempoPracticando"];
-      document.getElementById('dojang').value = alumno["dojang"];
-      document.getElementById('nombreProfesor').value = alumno["nombreProfesor"];
-      document.getElementById('fechaExamenAnterior').value = alumno["fechaExamenAnterior"];
-      document.getElementById('fechaExamen').value = alumno["fechaExamen"];
+        const alumno = snapshot.val();
+        if (alumno) {
+            document.getElementById('numeroCertificado').value = alumno["numeroCertificado"];
+            document.getElementById('gradoActual').value = alumno["gradoActual"];
+            document.getElementById('gradoParaSubir').value = alumno["gradoParaSubir"];
+            document.getElementById('radioNino').checked = alumno["radioNino"];
+            document.getElementById('radioAdulto').checked = alumno["radioAdulto"];
+            document.getElementById('nombres').value = alumno["nombre"];
+            document.getElementById('apellidoPaterno').value = alumno["apellidoPaterno"];
+            document.getElementById('apellidoMaterno').value = alumno["apellidoMaterno"];
+            document.getElementById('fechaNacimiento').value = alumno["fechaNacimiento"];
+            document.getElementById('edad').value = alumno["edad"];
+            document.getElementById('direccion').value = alumno["direccion"];
+            document.getElementById('telefono').value = alumno["telefono"];
+            document.getElementById('ocupacion').value = alumno["ocupacion"];
+            document.getElementById('fechaIngreso').value = alumno["fechaIngreso"];
+            document.getElementById('tiempoPracticando').value = alumno["tiempoPracticando"];
+            document.getElementById('dojang').value = alumno["dojang"];
+            document.getElementById('nombreProfesor').value = alumno["nombreProfesor"];
+            document.getElementById('fechaExamenAnterior').value = alumno["fechaExamenAnterior"];
+            document.getElementById('fechaExamen').value = alumno["fechaExamen"];
+        }
     });
-  }
+}
 
-  const enviarForm = document.getElementById('enviarForm');
-  enviarForm.addEventListener('click', () => {
-    let ubicacion = ref(database, `alumnos/${editarAlumnoID}`);
-    console.log(editarAlumnoID);
-    set(ubicacion, {
-    });
-  });
+const enviarForm = document.getElementById('enviarForm');
+enviarForm.addEventListener('click', () => {
+    if (editarAlumnoID) {
+        let ubicacion = ref(database, `alumnos/${editarAlumnoID}`);
+        set(ubicacion, {
+            numeroCertificado: document.getElementById('numeroCertificado').value,
+            gradoActual: document.getElementById('gradoActual').value,
+            gradoParaSubir: document.getElementById('gradoParaSubir').value,
+            radioNino: document.getElementById('radioNino').checked,
+            radioAdulto: document.getElementById('radioAdulto').checked,
+            nombre: document.getElementById('nombres').value,
+            apellidoPaterno: document.getElementById('apellidoPaterno').value,
+            apellidoMaterno: document.getElementById('apellidoMaterno').value,
+            fechaNacimiento: document.getElementById('fechaNacimiento').value,
+            edad: document.getElementById('edad').value,
+            direccion: document.getElementById('direccion').value,
+            telefono: document.getElementById('telefono').value,
+            ocupacion: document.getElementById('ocupacion').value,
+            fechaIngreso: document.getElementById('fechaIngreso').value,
+            tiempoPracticando: document.getElementById('tiempoPracticando').value,
+            dojang: document.getElementById('dojang').value,
+            nombreProfesor: document.getElementById('nombreProfesor').value,
+            fechaExamenAnterior: document.getElementById('fechaExamenAnterior').value,
+            fechaExamen: document.getElementById('fechaExamen').value,
+        }).then(() => {
+            alert('Alumno actualizado correctamente');
+        }).catch((error) => {
+            console.error('Error al actualizar el alumno:', error);
+        });
+    }
+});
